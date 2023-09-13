4fba669a912b11380cc85bfe46dbd9ad391e8814
Gage Hilyard <gagehilyard@gmail.com>
2023-09-13 16:33:19 -0600

    updated views

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,17 @@
-from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user
-from app import app
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
+from datetime import datetime
+from app import app, db
+from app.forms import LoginForm, SignupForm
 from app.models import User
+
+
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+    return render_template('index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -19,6 +28,9 @@ def login():
             flash('Invalid password')
             return redirect(url_for('login'))
         login_user(user, remeber=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
@@ -27,3 +39,34 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = SignupForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data,
+                    email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Thank you for signing up!')
+        return redirect(url_for('login'))
+    return render_template('sign_up.html', title='Sign up', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+
+    return render_template('user.html', user=user)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
