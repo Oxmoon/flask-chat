80a33b2ec71ca5f3496144b6ccc996b4f7d74ce0
Gage Hilyard <gagehilyard@gmail.com>
2023-09-15 22:14:18 -0600

    refactor

--- app/models.py
+++ app/models.py
@@ -1,20 +1,15 @@
 from datetime import datetime
-from . import db
-from app import login
 from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask import current_app
 from flask_login import UserMixin
+from app import login, db
 
 
 user_room = db.Table('user_room',
                      db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
                      db.Column('room_id', db.Integer, db.ForeignKey('room.id'))
                      )
-
-
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
 
 
 class Message(db.Model):
@@ -33,8 +28,8 @@ class User(UserMixin, db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
+    username = db.Column(db.String(150), unique=True)
     password_hash = db.Column(db.String(150))
-    username = db.Column(db.String(150))
     messages = db.relationship('Message', backref='user', lazy='dynamic')
     rooms = db.relationship('Room',
                             secondary=user_room,
@@ -50,16 +45,36 @@ class User(UserMixin, db.Model):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
-    def __init__(self, email, username, password_hash):
+    def __init__(self, email, username):
         self.email = email
         self.username = username
-        self.password_hash = password_hash
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def join(self, room):
+        if not self.has_joined(room):
+            self.rooms.append(room)
+
+    def leave(self, room):
+        if self.has_joined(room):
+            self.rooms.remove(room)
+
+    def has_joined(self, room):
+        return self.rooms.filter(user_room.c.room_id == room.id).count() > 0
+
+    def joined_rooms(self):
+        joined = Room.query.join(
+            user_room,
+            (user_room.c.room_id == Room.id)).filter(user_room.c.user_id == self.id)
+        return joined
+
+    @login.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
 
 class Room(db.Model):
@@ -70,3 +85,6 @@ class Room(db.Model):
 
     def __repr__(self):
         return '<Room %r>' % self.name
+
+    def __init__(self, name):
+        self.name = name
