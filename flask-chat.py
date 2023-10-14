c9b2afe53d116e3f7861cc343d7ac4505cadf0f3
Gage Hilyard <gagehilyard@gmail.com>
2023-10-14 14:44:28 -0600

    moved general room creation

--- flask-chat.py
+++ flask-chat.py
@@ -6,6 +6,17 @@ from flask_migrate import Migrate
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
+with app.app_context():
+    db.create_all()
+
+    # ensure general room is created
+    general_room = Room.query.filter_by(name="General").first()
+    if general_room is None:
+        general_room = Room(name="General",
+                            private=False)
+        db.session.add(general_room)
+        db.session.commit()
+
 
 @app.shell_context_processor
 def make_shell_context():
