80bc868ba9bb768960605eb2199c16f5321e22c0
Gage Hilyard <gagehilyard@gmail.com>
2023-10-16 20:04:25 -0600

    configured gunicorn

--- boot.sh
+++ boot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 source .venv/bin/activate
 flask deploy
-exec gunicorn -b 0.0.0.0:5000 --access-logfile - --error-logfile - flask-chat:app
+exec gunicorn -b 0.0.0.0:5000 --worker-class=gevent --workers 4 --access-logfile - --error-logfile - flask-chat:app
